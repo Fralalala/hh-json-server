b696db62324351d105e4a5c05556da9d226f4bc5
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-01 20:27:42 -0700

    Added graphql ui

--- server.js
+++ server.js
@@ -9,4 +9,12 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
+server.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
 server.listen(port);
