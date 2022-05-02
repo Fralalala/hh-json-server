0a86188b95cb6949836a9b889afa3ae0a3ab8543
Rolando Morales <rolando.moralesjr21@gmail.com>
2022-05-01 21:02:03 -0700

    added delete all

--- server.js
+++ server.js
@@ -8,11 +8,33 @@ const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 server.use(cors());
 server.use(express.json());
 server.use(middlewares);
 server.use(router);
+
+server.delete("/colors", async (req, res) => {
+  try {
+    const { ids } = req.body;
+
+    let url = "https://hh-json-server.herokuapp.com/colors";
+
+    const apiCalls = [];
+
+    ids.forEach((id) => {
+      apiCalls.push(axios.delete(url + id));
+    });
+
+    await Promise.all(apiCalls);
+
+    res.send(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 
 server.use(
   "/graphql",
